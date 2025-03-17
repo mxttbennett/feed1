46f70b098f6db44bcfab3824c8c6f12f37f60489
Matt Bennett <mattbennett154@gmail.com>
2025-03-17 19:36:57 -0400

    migraitons

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  development: {
+    // Use the same config you're using in your app
+    dialect: 'sqlite',
+    storage: './.data/database.sqlite',
+    // Or for other databases:
+    // username: 'your_username',
+    // password: 'your_password',
+    // database: 'your_database',
+    // host: 'localhost',
+    // dialect: 'mysql' // or 'postgres', etc.
+  }
+}; 
